324da3222d5c5edb32b191cc4d7bf47e72ac838e
prsn <renaud.steun@capgemini.com>
2020-04-16 14:58:20 +0200

    TP chat 01 done

---
+++ AspNet-TP/AspNet-TP01-Module05/Controllers/ChatController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Database;
+using TP.Models;
+
+namespace AspNet_TP01_Module05.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(FakeDBCat.Instance.ListeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            Chat chat = FakeDBCat.Instance.ListeChats.First(c => c.Id == id);
+
+            return chat != null ? View(chat) : (ActionResult) RedirectToAction("Index");
+        }
+
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Chat chat = FakeDBCat.Instance.ListeChats.First(c => c.Id == id);
+
+            return chat != null ? View(chat) : (ActionResult) RedirectToAction("Index");
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Chat chat = FakeDBCat.Instance.ListeChats.First(c => c.Id == id);
+
+                if (chat != null)
+                {
+                    FakeDBCat.Instance.ListeChats.Remove(chat);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AspNet-TP/AspNet-TP01-Module05/Database/FakeDBCat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP.Models;
+
+namespace TP.Database
+{
+    public class FakeDBCat
+    {
+        private static FakeDBCat _instance;
+        static readonly object instanceLock = new object();
+
+        private FakeDBCat()
+        {
+            GetMeuteDeChats();
+        }
+
+        public static FakeDBCat Instance
+        {
+            get
+            {
+                if (_instance == null
+                ) //Les locks prennent du temps, il est préférable de vérifier d'abord la nullité de l'instance.
+                {
+                    lock (instanceLock)
+                    {
+                        if (_instance == null) //on vérifie encore, au cas où l'instance aurait été créée entretemps.
+                            _instance = new FakeDBCat();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private List<Chat> listeChats = new List<Chat>();
+
+        public List<Chat> ListeChats
+        {
+            get { return listeChats; }
+        }
+
+
+        private void GetMeuteDeChats()
+        {
+            var i = 1;
+            {
+                listeChats.Add(new Chat {Id = i++, Nom = "Felix", Age = 3, Couleur = "Roux"});
+                listeChats.Add(new Chat {Id = i++, Nom = "Minette", Age = 1, Couleur = "Noire"});
+                listeChats.Add(new Chat {Id = i++, Nom = "Miss", Age = 10, Couleur = "Blanche"});
+                listeChats.Add(new Chat {Id = i++, Nom = "Garfield", Age = 6, Couleur = "Gris"});
+                listeChats.Add(new Chat {Id = i++, Nom = "Chatran", Age = 4, Couleur = "Fauve"});
+                listeChats.Add(new Chat {Id = i++, Nom = "Minou", Age = 2, Couleur = "Blanc"});
+                listeChats.Add(new Chat {Id = i, Nom = "Bichette", Age = 12, Couleur = "Rousse"});
+            }
+            ;
+        }
+    }
+}
